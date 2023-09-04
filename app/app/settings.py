ded794905b933fd9b78b7841f211533af31c34f7
saqlain-haider1 <saqlain.haider@tintash.com>
2023-09-04 16:03:22 +0500

    WHAT: Code polishing and refactoring

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,7 +76,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'authentication.middleware.JWTAuthMiddleware'
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -100,22 +99,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config("DB_NAME"),
         'USER': config("DB_USER"),
-        # 'PASSWORD': config("DB_PASSWORD"),
         'HOST': config("DB_HOST"),
         'PORT': config("DB_PORT"),
     }
