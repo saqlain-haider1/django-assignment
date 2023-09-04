d70a53f4e5e3014bbf7fd7b41c6a3893e5bb2747
saqlain-haider1 <saqlain.haider@tintash.com>
2023-09-04 11:36:43 +0500

    WHAT: Removed custom User app from the project. WHY: Already using built in User from Django auth module.

--- app/app/settings.py
+++ app/app/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'like',
     'song',
     'tag',
-    'user',
     'rest_framework',
     'rest_framework_simplejwt',
     'authentication',
--- app/user/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class User(models.Model):
-    username = models.CharField(max_length=25)
-    password = models.CharField(max_length=8)
-    email = models.EmailField()
